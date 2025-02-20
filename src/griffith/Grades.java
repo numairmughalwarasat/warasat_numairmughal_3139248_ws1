13395bf6daff0fc847d31727bd016df979c870b9
Numair Mughal Warasat <numairmughal.warasat@student.griffith.ie>
2025-02-20 20:59:10 +0000

    Initial commit

---
+++ src/griffith/Grades.java
@@ -0,0 +1,20 @@
+package griffith;
+
+public class Grades {
+
+    public int gradesMax(int[] grades) {
+        return 0; // Placeholder return value
+    }
+
+    public int gradesTotal(int[] grades) {
+        return 0; // Placeholder return value
+    }
+
+    public double gradesAverage(int[] grades) {
+        return 0.0; // Placeholder return value
+    }
+
+    public int countFails(int[] grades, int minGrade) {
+        return 0; // Placeholder return value
+    }
+}
---
+++ src/griffith/GradesTest.java
@@ -0,0 +1,27 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class GradesTest {
+
+    @Test
+    void testGradesMax() {
+        // Test cases will be added here
+    }
+
+    @Test
+    void testGradesTotal() {
+        // Test cases will be added here
+    }
+
+    @Test
+    void testGradesAverage() {
+        // Test cases will be added here
+    }
+
+    @Test
+    void testCountFails() {
+        // Test cases will be added here
+    }
+}
