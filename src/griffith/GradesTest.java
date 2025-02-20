ac8323a6b69305d821a02c9c163a11ef95e22592
Numair Mughal Warasat <numairmughal.warasat@student.griffith.ie>
2025-02-20 21:37:27 +0000

    i made changes by adding test cases on GradesTest.java

--- src/griffith/GradesTest.java
+++ src/griffith/GradesTest.java
@@ -7,21 +7,41 @@ class GradesTest {
 
     @Test
     void testGradesMax() {
-        // Test cases will be added here
+        Grades grades = new Grades();
+        
+        // Test with positive grades
+        assertEquals(95, grades.gradesMax(new int[]{90, 85, 95, 78}));
+        
+        // Test with negative grades
+        assertEquals(0, grades.gradesMax(new int[]{-10, -20, -5, 0}));
+        
+        // Test with null array (expected to throw NullPointerException)
+        assertThrows(NullPointerException.class, () -> grades.gradesMax(null));
     }
 
     @Test
     void testGradesTotal() {
-        // Test cases will be added here
+        Grades grades = new Grades();
+        
+        // Test with positive grades
+        assertEquals(350, grades.gradesTotal(new int[]{90, 85, 95, 80}));
+        
+        // Test with negative grades
+        assertEquals(-35, grades.gradesTotal(new int[]{-10, -20, -5, 0}));
+        
+        // Test with null array (expected to throw NullPointerException)
+        assertThrows(NullPointerException.class, () -> grades.gradesTotal(null));
     }
 
     @Test
     void testGradesAverage() {
-        // Test cases will be added here
-    }
-
-    @Test
-    void testCountFails() { 
-        // Test cases will be added her
-    }
-}
+        Grades grades = new Grades();
+        
+        // Test with positive grades
+        assertEquals(87.5, grades.gradesAverage(new int[]{90, 85, 95, 80}), 0.01);
+        
+        // Test with negative grades
+        assertEquals(-8.75, grades.gradesAverage(new int[]{-10, -20, -5, 0}), 0.01);
+        
+        // Test with null array (expect
+    }}
