34d9ddcb0b085efda2715e2575f7775e75ba679c
Numair Mughal Warasat <numairmughal.warasat@student.griffith.ie>
2025-02-20 22:13:07 +0000

    implemented the file Words.java with code

--- src/griffith/Words.java
+++ src/griffith/Words.java
@@ -2,4 +2,29 @@ package griffith;
 
 public class Words {
 
+    // Attribute to store the letters
+    private char[] letters;
+
+    // Constructor to initialize the letters attribute
+    public Words(char[] letters) {
+        this.letters = letters;
+    }
+
+    // Stub method to check if the word contains a specific symbol
+    public boolean contains(char symbol) {
+        // This method will be implemented later
+        return false;
+    }
+
+    // Stub method to return the length of the word
+    public int length() {
+        // This method will be implemented later
+        return 0;
+    }
+
+    // Stub method to get the letters attribute
+    public char[] getLetters() {
+        // This method will be implemented later
+        return letters;
+    }
 }
